a91020bcfa3cbf51786b8929b7901ebf2608bd0f
jewel3r <99684118+jewel3r@users.noreply.github.com>
2024-03-04 21:30:07 +0300

    Add files via upload

---
+++ showandclear.sql
@@ -0,0 +1,16 @@
+select 'Clients:';
+select * from clients;
+select 'Contracts';
+select * from contracts;
+select 'Employees';
+select * from employees;
+select 'Workplaces';
+select * from workplaces;
+select 'Services';
+select * from services;
+
+drop table contracts;
+drop table employees;
+drop table clients;
+drop table workplaces;
+drop table services;
