b7104361c0f97cb67eadde8ac042a4399431552c
jewel3r <99684118+jewel3r@users.noreply.github.com>
2024-05-29 10:05:22 +0300

    Delete src directory

--- src/main/java/com/example/luck/CommonEntity.java
+++
@@ -1,6 +0,0 @@
-package com.example.luck;
-
-public interface CommonEntity<ID> {
-    ID getId();
-    void setId(ID id);
-}
--- src/main/java/com/example/luck/dao/ClientDAO.java
+++
@@ -1,7 +0,0 @@
-package com.example.luck.dao;
-
-import com.example.luck.Client;
-
-public interface ClientDAO extends DAO<Client>{
-
-}
